a244ab9cad0c05a32749eb598caf723f19a9f7fd
SlogoSniper <joshuaming2004@gmail.com>
2023-04-12 12:51:29 -0400

    Fixed code to compile

--- C++/Textbook_Exercises/Chapter_2/outputing_variables.cpp
+++ C++/Textbook_Exercises/Chapter_2/outputing_variables.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
 int hour, minute;
 char colon;
 
-hour = 11;
-minute = 59;
+hour = 12;
+minute = 51;
 colon = ':';
 
 cout << "The current time is " << hour << colon << minute << endl;
+return 0;
+}
