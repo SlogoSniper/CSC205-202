25ec091345861f7ebc6c02f8d3017520a5e012d3
SlogoSniper <joshuaming2004@gmail.com>
2023-04-14 08:38:05 -0400

    Fixed Chapter 3 Exercise 2

--- C++/Textbook_Exercises/Chapter_3/print_twice.cpp
+++ C++/Textbook_Exercises/Chapter_3/print_twice.cpp
@@ -1,16 +1,17 @@
 // Will not work because trying to pass non-char into a char argument
+// Will work if function is changed to string input and then convert non-strings into strings
 
 #include <iostream>
 using namespace std;
 
-void print_twice(char phil) {
+void print_twice(string phil) {
     cout << phil << phil << endl;
 }
 
 int main() {
-    print_twice('a');
-    print_twice(3);
-    print_twice(3.23);
+    print_twice("a");
+    print_twice(to_string(3));
+    print_twice(to_string(3.23));
     print_twice("hello");
     return 0;
 }
